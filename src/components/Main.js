5b126c8a79a3839072750fc4902ff4ef4d70905a
jamesdoh0109 <magicjcdp@gmail.com>
2021-06-29 22:22:39 +0900

    minor changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,36 +6,35 @@ import background1 from '../img/bg_gradient.png';
 
 export default class Main extends Component {
     render() {
-        // //styling of text at top
-        // const styleBackground = {
-        //     color: "white",
-        //     fontFamily: "Gilroy-Light",
-        //     fontSize: "x-large",
+        //styling of text at top
+        const styleBackground = {
+            color: "white",
+            fontFamily: "Gilroy-Light",
+            fontSize: "x-large",
             
-        // }
+        }
 
-        // compotentDidMount(){
-        //     const myData = this.state.contacts
-        //     .sort((a, b) => a.name.localeCompare(b.name))
-        //     map((item, i) => <List key={i} data={item} />);
-        // };
+        compotentDidMount(){
+            const myData = this.state.contacts
+            .sort((a, b) => a.name.localeCompare(b.name))
+            map((item, i) => <List key={i} data={item} />);
+        };
 
 
         return (
-            <div>main</div>
-        //     <div style={{ backgroundImage: 'url(${background1})'}}>
-        //         <h1 className ="h3 mb-3 text-center" style={styleBackground}>Employees</h1>
-        //         <ul> 
-        //             {
-        //                 this.state.contacts.map((items, i) => <List key={i} data={item} />)
-        //             }
-        //         </ul>
+            <div style={{ backgroundImage: 'url(${background1})'}}>
+                <h1 className ="h3 mb-3 text-center" style={styleBackground}>Employees</h1>
+                <ul> 
+                    {
+                        this.state.contacts.map((items, i) => <List key={i} data={item} />)
+                    }
+                </ul>
 
 
 
 
-        //         <p className="mt-5 mb-3 text-muted">Copyright © 2011-2021, Share IT and its related companies. All rights reserved.</p>
-        //     </div>
+                <p className="mt-5 mb-3 text-muted">Copyright © 2011-2021, Share IT and its related companies. All rights reserved.</p>
+            </div>
         )
     }
 }
